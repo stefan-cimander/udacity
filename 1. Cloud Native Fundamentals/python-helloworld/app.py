2488e9864daafa734d3e65a5247fa557eb0e9552
Stefan Cimander <stefan.cimander@me.com>
2021-05-19 14:00:26 +0200

    Configure logging for the sample pyhton flask app

--- 1. Cloud Native Fundamentals/python-helloworld/app.py
+++ 1. Cloud Native Fundamentals/python-helloworld/app.py
@@ -1,24 +1,30 @@
 from flask import Flask, json
+import logging
 app = Flask(__name__)
+
+logging.basicConfig(filename="app.log", level=logging.DEBUG, format=f"%(asctime)s, %(message)s")
 
 @app.route("/")
 def hello():
+    logging.debug("/ was reached")
     return "Hello World!"
 
 @app.route("/status")
 def status():
+    logging.debug("/status was reached")
     return app.response_class(
-        response = json.dumps({"result": "OK - healthy"}),
-        status = 200,
-        mimetype = "application/json"
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype="application/json"
     );
 
 @app.route("/metrics")
 def metrics():
+    logging.debug("/metrics was reached")
     return app.response_class(
-        response = json.dumps({"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 23}}),
-        status = 200,
-        mimetype = "application/json"
+        response=json.dumps({"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 23}}),
+        status=200,
+        mimetype="application/json"
     );
 
 if __name__ == "__main__":
