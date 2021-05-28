c867829ed80c2c10df63e3742dd3ff976623fa6c
Stefan Cimander <stefan.cimander@me.com>
2021-05-28 18:59:04 +0200

    Add health and metrics endpoints as well as logging to techtrends app

--- Cloud Native Application Architecture/1. Cloud Native Fundamentals/Project/techtrends/app.py
+++ Cloud Native Application Architecture/1. Cloud Native Fundamentals/Project/techtrends/app.py
@@ -1,20 +1,32 @@
 import sqlite3
+import logging
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
+    db_connection_count += 1
+
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     return connection
 
+# Function to get all posts
+def get_all_posts():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    return posts
+
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     connection.close()
     return post
 
@@ -25,10 +37,20 @@ app.config['SECRET_KEY'] = 'your secret key'
 # Define the main route of the web application 
 @app.route('/')
 def index():
-    connection = get_db_connection()
-    posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
+    posts = get_all_posts()
     return render_template('index.html', posts=posts)
+
+# Define a health endpoint for the application
+@app.route('/healthz')
+def health():
+    return jsonify({'result': 'OK - healthy'}), 200
+
+# Define a metrics endpoint for the application
+@app.route('/metrics')
+def metrics():
+    post_count = len(get_all_posts())
+    return jsonify({'db_connection_count': db_connection_count, 'post_count': post_count})
+
 
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
@@ -36,13 +58,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        logging.error('Article with id %i not found', post_id)
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        logging.info('Article "%s" retrieved', post['title'])
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logging.info('About Us page retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -56,15 +81,17 @@ def create():
             flash('Title is required!')
         else:
             connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
             connection.commit()
             connection.close()
+
+            logging.info('New article "%s" created', title)
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
-# start the application on port 3111
+# Configure the logging and start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(level=logging.DEBUG, format=f"%(asctime)s, %(message)s")
+    app.run(host='0.0.0.0', port='3111')
